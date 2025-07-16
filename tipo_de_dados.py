c20a331245de6f9233191768babd73568928c66f
igor rafael <igor.briano1998@gmail.com>
2025-07-15 22:30:05 -0300

    arquivo com tipos de variaveis

---
+++ tipo_de_dados.py
@@ -0,0 +1,36 @@
+# Dinamicamente tipada (não precisa declarar o tipo,
+# elas podem mudar durante a execução do programa)
+# inteiro - int
+# ponto flutuante - float
+# complexo - complex
+# String - str
+# Boolean - bool
+# List - list
+# tuple
+# dictionary - dic
+
+# int, float, String, Boolean = em java
+
+# COMPLEX ###
+# numeros complexos, é usado o sufixo 'j' para a parte imaginaria
+# ex: a = 10+5j -> type(a) = complex
+
+# ex: a = complex(2, 5)
+# print(a) -> 2+5j
+
+# LIST ###
+# BASICAMENTE O ARRAY
+# ex: lista = ["nome", "numero", "texto"]
+
+# TUPLA ###
+# parecido com o LIST, porem eu conteudo é imutavel
+# ex: lista =(1, 3, 5, 7)
+
+# DICT ###
+# Agrupa elementos através da estrutura de chave e valor
+# ex: altura = {'Amanda': 1.65, 'Ana': 1.60, 'João': 1.70}
+
+
+# MUDANÇA DE TIPO DE VARIAVEL
+# para mudar um tipo de variavel basta apenas indicar qual o tipo quer fazer a conversão
+# ex: idade = 18 (inicialemnte uma int) -> idade = float(idade) // convertemos de int para float
